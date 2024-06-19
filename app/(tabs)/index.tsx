b9cc0cc941144d9cdaffdf2761c34cf10cf0ad96
fatih <fatihkkokcu@outlook.com>
2024-06-19 03:33:51 +0300

    First commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,40 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import React from 'react';
+import { StyleSheet, ScrollView, TextInput, Button } from 'react-native';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+
+const genAI = new GoogleGenerativeAI('AIzaSyDbd2H8lIN78X6epgF0y4uKZj2BEvnHZmc');
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+const chat = model.startChat()
 
 export default function HomeScreen() {
+  const [text, setText] = React.useState('');
+  const [inputText, setInputText] = React.useState('');
+
+  const sendText = async () => {
+    const result = await chat.sendMessageStream(inputText);
+    let streamText = '';
+    for await (const chunk of result.stream) {
+      const chunkText = await chunk.text();
+      streamText += chunkText;
+      setText(streamText)
+      console.log(streamText)
+    }
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView>
+      <ThemedText>{text}</ThemedText>
+      <TextInput
+        value={inputText}
+        onChangeText={setInputText}
+        placeholder="Mesajınızı yazın"
+      />
+      <Button title='Send' onPress={sendText}/>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  
 });
